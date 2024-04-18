0dd9a810f9779719469066be5a4649b4ab70c39a
Storm2601 <kevinguedon2601@gmail.com>
2024-04-18 09:55:52 +0200

    divides the arguments and stocks into an array

---
+++ main.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - fonction main
+ * @ac: number of arguments
+ * @argv: array of arguments
+ * Return: Always 0
+*/
+int main(int ac, char **argv)
+{
+
+	(void)ac;
+	(void)argv;
+
+	_printlineTyped();
+
+	return (0);
+}
