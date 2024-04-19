0a5bd213287d85fc391c5d4583b48900aca6e243
root <root@Kvin2601>
2024-04-19 14:00:45 +0200

    add prototypes for files shell.c, tokenization.c, get_the_location

---
+++ main.h
@@ -0,0 +1,23 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+
+extern char **environ;
+
+
+int _printlineTyped(void);
+int startShell(void);
+char **process_command(char *lineptr);
+void exec_command(char **tokens);
+int exit_shell(char *command);
+void printenv(char **environ);
+char *get_location(char *command);
+
+#endif
