3bbf1741c1e51cfc4cb11f5242693bddc0d03c9b
root <root@Kvin2601>
2024-04-19 13:05:40 +0200

    Implement the env built-in, that prints the current environment

---
+++ main.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - function main
+ * @ac: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: Always 0
+*/
+
+int main(int ac, char **argv)
+{
+
+	(void)ac;
+	(void)argv;
+
+	_printlineTyped();
+
+	return (0);
+}
